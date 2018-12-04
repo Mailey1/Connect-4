bd1cd0385832622f949538719419693712ddc610
Mike <mike.bailey122@gmail.com>
2018-12-04 13:02:20 -0800

    Discovered and fixed an error in checking for a winner. Added the board evaluation function.

--- Connect4Board.py
+++ Connect4Board.py
@@ -1,3 +1,5 @@
+import copy
+
 class Connect4Board:
 	def __init__(self, size):
 		self.num_rows = size - 1
@@ -6,7 +8,14 @@ class Connect4Board:
 		self.column_capacity = {x : 0 for x in range(size)}
 		self.num_turns = 0;
 		
-		
+	# Copies another instance of a board...
+	def copy_board(self, new_board):
+		self.num_rows = new_board.num_rows
+		self.num_cols = new_board.num_cols
+		self.board = copy.deepcopy(new_board.board)
+		self.column_capacity = new_board.column_capacity.copy()
+		self.num_turns = new_board.num_turns
+	
 	# Place piece in the column called. The value in column capacity corresponds to the row where the piece will fall when placed in 
 	# that column. If that column has a capacity of 7, it is full and the piece cannot be placed there.
 	def insert_piece(self, column_number, colour):
@@ -16,9 +25,39 @@ class Connect4Board:
 			return False
 		self.board[column_number][self.column_capacity[column_number]] = colour;
 		self.column_capacity[column_number] += 1
+		self.num_turns += 1
 		return True
 		
+	# Assume that the board currently has no connect 4's. Place the computer's coloured piece in every single open (0) cell
+	# on the board, then check if the creates a connect 4. Then do the same for the player's coloured pieces, and the heuristic
+	# is the difference.
+	def eval_board(self, computer_colour, player_colour):
+		computer_wins = 0
+		player_wins = 0
+		# Use a board copy just to be safe.
+		board2 = Connect4Board(1)
+		board2.copy_board(self)
 		
+		# Iterate over every entry in board2, if it's empty then it's cell is a 0. If that's the case fill it with a piece
+		# of the computer's colour and check if it's a win. Then do the same for the player colour. Then change it back to a 0.
+		for i in range(board2.num_cols):
+			for j in range(board2.num_rows):
+				if board2.board[i][j] != 0:
+					continue
+				board2.board[i][j] = computer_colour
+				if board2.has_winner(computer_colour):
+					computer_wins += 1
+					print("Winning board for computer")
+					print(board2)
+				board2.board[i][j] = player_colour
+				if board2.has_winner(player_colour):
+					player_wins += 1
+					print("Winning board for player")
+					print(board2)
+				board2.board[i][j] = 0
+				
+		return computer_wins - player_wins
+	
 	# iterate across the columns (second index), and print out the entries of the rows from the bottom up.
 	def __str__(self):
 		output = ""
@@ -36,22 +75,22 @@ class Connect4Board:
 		for i in range(self.num_cols):
 			for j in range(self.num_rows - 3):
 				if (colour == self.board[i][j] == self.board[i][j+1] == self.board[i][j+2] == self.board[i][j+3]):
-					return "A player has won"
+					return True
 		# Now iterate through each row
 		for j in range(self.num_rows):
 			for i in range(self.num_cols - 3):
 				if (colour == self.board[i][j] == self.board[i+1][j] == self.board[i+2][j] == self.board[i+3][j]):
-					return "A player has won"
+					return True
 		# Now check forward diagonals
 		for i in range(self.num_cols - 3):
 			for j in range(self.num_rows - 3):
 				if (colour == self.board[i][j] == self.board[i+1][j+1] == self.board[i+2][j+2] == self.board[i+3][j+3]):
-					return "A player has won"
+					return True
 		# Now the back diagonals
 		for i in range(self.num_cols - 1, 2, -1):
-			for j in range(self.num_rows - 1, 2, -1):
-				if (colour == self.board[i][j] == self.board[i-1][j-1] == self.board[i-2][j-2] == self.board[i-3][j-3]):
-					return "A player has won"
-		return "Nobody has won"
+			for j in range(self.num_rows - 3):
+				if (colour == self.board[i][j] == self.board[i-1][j+1] == self.board[i-2][j+2] == self.board[i-3][j+3]):
+					return True
+		return False
 		
 	
